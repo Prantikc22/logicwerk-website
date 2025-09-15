cc0f3666bdd807c63374850611fcb8ddd6b1cc90
prantikc22 <prantikchatterjee22@gmail.com>
2025-09-15 17:43:37 +0530

    GTM tag added for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,26 @@ export default function RootLayout({
       </Head>
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className} suppressHydrationWarning>
+          {/* Google Tag Manager (noscript) - immediately after opening body */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-544ZBTZQ"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+          {/* Google Tag Manager - initialize */}
+          <Script id="gtm-init" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[]; w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-544ZBTZQ');
+            `}
+          </Script>
           {/* GoodFirms widget script loaded globally as recommended */}
           <Script src="https://assets.goodfirms.co/assets/js/widget.min.js" strategy="afterInteractive" />
           <GoogleAnalytics />
